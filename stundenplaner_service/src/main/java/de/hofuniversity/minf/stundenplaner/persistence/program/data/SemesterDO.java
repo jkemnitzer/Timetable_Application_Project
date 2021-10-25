6ae3f1dad0ebe0e5b26f427280f4cbb569c318fe
Noah Lehmann <noah.lehmann@hof-university.de>
2021-10-25 14:18:41 +0200

    feature/6043_datenobjekt_studiengang: added sequence

--- stundenplaner_service/src/main/java/de/hofuniversity/minf/stundenplaner/persistence/program/data/SemesterDO.java
+++ stundenplaner_service/src/main/java/de/hofuniversity/minf/stundenplaner/persistence/program/data/SemesterDO.java
@@ -9,9 +9,11 @@ import lombok.Setter;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Getter
@@ -24,6 +26,8 @@ public class SemesterDO {
 
     @Id
     @Column(name = "id")
+    @GeneratedValue(generator = "t_semester_seq")
+    @SequenceGenerator(name = "t_semester_seq", sequenceName = "t_semester_seq", allocationSize = 1)
     private Long id;
 
     @Column(name = "number")
