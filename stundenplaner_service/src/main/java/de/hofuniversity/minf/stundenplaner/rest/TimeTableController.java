e436af28114d8128f3dd84019f8c599458d2e7d0
Noah Lehmann <noah.lehmann@hof-university.de>
2021-11-19 09:51:19 +0100

    feature/6282_login_service: add Excel to Timetable

--- stundenplaner_service/src/main/java/de/hofuniversity/minf/stundenplaner/rest/TimeTableController.java
+++ stundenplaner_service/src/main/java/de/hofuniversity/minf/stundenplaner/rest/TimeTableController.java
@@ -3,6 +3,7 @@ package de.hofuniversity.minf.stundenplaner.rest;
 import de.hofuniversity.minf.stundenplaner.service.boundary.TimeTableService;
 import de.hofuniversity.minf.stundenplaner.service.to.LessonTO;
 import de.hofuniversity.minf.stundenplaner.service.to.TimeTableTO;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -63,8 +67,39 @@ public class TimeTableController {
     }
 
     @DeleteMapping("/lesson/{id}")
-    public ResponseEntity<LessonTO> deleteLessonById(@PathVariable("id") Long id){
+    public ResponseEntity<LessonTO> deleteLessonById(@PathVariable("id") Long id) {
         return ResponseEntity.ok(service.deleteLesson(id));
     }
+
+    @GetMapping(value = "/export", produces = "application/octet-stream")
+    public void getExport(HttpServletResponse response) throws IOException {
+        this.executeExcelExport(response);
+    }
+
+    @GetMapping(value = "/{versionId}/export", produces = "application/octet-stream")
+    public void getVersionExport(HttpServletResponse response, @PathVariable("versionId") Long versionId) throws IOException {
+        this.executeExcelExport(response, versionId);
+    }
+
+    private void executeExcelExport(HttpServletResponse response) throws IOException {
+        this.executeExcelExport(response, null);
+    }
+
+    private void executeExcelExport(HttpServletResponse response, Long versionId) throws IOException {
+        Workbook workbook;
+        if (versionId == null) {
+            workbook = service.exportAll();
+        } else {
+            workbook = service.exportVersion(versionId);
+        }
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=timetable_version_" + versionId + ".xlsx";
+        response.setHeader(headerKey, headerValue);
+        ServletOutputStream outputStream = response.getOutputStream();
+        workbook.write(outputStream);
+        workbook.close();
+        outputStream.close();
+    }
+
 
 }
--- stundenplaner_service/src/test/java/de/hofuniversity/minf/stundenplaner/service/UserServiceTest.java
+++ stundenplaner_service/src/test/java/de/hofuniversity/minf/stundenplaner/service/UserServiceTest.java
@@ -1,6 +1,6 @@
 package de.hofuniversity.minf.stundenplaner.service;
 
-import de.hofuniversity.minf.stundenplaner.common.NotFoundException;
+import de.hofuniversity.minf.stundenplaner.common.exception.NotFoundException;
 import de.hofuniversity.minf.stundenplaner.persistence.role.RoleRepository;
 import de.hofuniversity.minf.stundenplaner.persistence.user.UserRepository;
 import de.hofuniversity.minf.stundenplaner.persistence.user.data.StatusEnum;
