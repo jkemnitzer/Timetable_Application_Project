e81362684297591c78375212edc591e56966a3a0
Noah Lehmann <noah.lehmann@hof-university.de>
2021-11-19 11:11:56 +0100

    feature/6282_login_service: format timetable export

--- stundenplaner_service/src/main/java/de/hofuniversity/minf/stundenplaner/common/excel/TimeTableWorkBookBuilder.java
+++ stundenplaner_service/src/main/java/de/hofuniversity/minf/stundenplaner/common/excel/TimeTableWorkBookBuilder.java
@@ -3,6 +3,10 @@ package de.hofuniversity.minf.stundenplaner.common.excel;
 import de.hofuniversity.minf.stundenplaner.service.to.LessonTO;
 import de.hofuniversity.minf.stundenplaner.service.to.TimeTableVersionTO;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 
@@ -10,6 +14,7 @@ import java.time.DayOfWeek;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Slf4j
 public class TimeTableWorkBookBuilder extends GenericWorkbookBuilder<LessonTO> {
@@ -25,7 +30,8 @@ public class TimeTableWorkBookBuilder extends GenericWorkbookBuilder<LessonTO> {
     @Override
     public TimeTableWorkBookBuilder addItemsToSheet(String sheetKey, List<LessonTO> items) {
         final Sheet sheet = super.getSheet(sheetKey);
-        this.addHeaders(sheet);
+        final List<String> titles = List.of("Wochentag", "Start", "Ende", "Raum", "Titel", "Dozent", "Typ", "Notiz");
+        this.addHeaders(sheet, titles);
         this.sortLessons(items).forEach(item -> {
             Row row = sheet.createRow(sheet.getPhysicalNumberOfRows());
             row.createCell(row.getPhysicalNumberOfCells()).setCellValue(DayOfWeek.of(item.getWeekdayNr()).toString());
@@ -37,6 +43,7 @@ public class TimeTableWorkBookBuilder extends GenericWorkbookBuilder<LessonTO> {
             row.createCell(row.getPhysicalNumberOfCells()).setCellValue(item.getLessonType());
             row.createCell(row.getPhysicalNumberOfCells()).setCellValue(item.getNote());
         });
+        IntStream.range(0, titles.size()).forEach(sheet::autoSizeColumn);
         return this;
     }
 
@@ -48,10 +55,13 @@ public class TimeTableWorkBookBuilder extends GenericWorkbookBuilder<LessonTO> {
         return this;
     }
 
-    private void addHeaders(Sheet sheet) {
+    private void addHeaders(Sheet sheet, List<String> headers) {
         final Row row = sheet.createRow(sheet.getPhysicalNumberOfRows());
-        final List<String> titles = List.of("Wochentag", "Start", "Ende", "Raum", "Titel", "Dozent", "Typ", "Notiz");
-        titles.forEach(title -> row.createCell(row.getPhysicalNumberOfCells()).setCellValue(title));
+        headers.forEach(title -> {
+            Cell cell = row.createCell(row.getPhysicalNumberOfCells());
+            cell.setCellValue(title);
+            this.setCellStyle(cell);
+        });
     }
 
     private List<LessonTO> sortLessons(List<LessonTO> list) {
@@ -60,6 +70,19 @@ public class TimeTableWorkBookBuilder extends GenericWorkbookBuilder<LessonTO> {
                 .thenComparing(LessonTO::getStartTime)
                 .thenComparing(LessonTO::getLectureTitle));
         return newList;
+    }
+
+    private void setCellStyle(Cell cell) {
+        final CellStyle style = cell.getSheet().getWorkbook().createCellStyle();
+        final BorderStyle borderStyle = BorderStyle.MEDIUM;
+        final Font font = cell.getSheet().getWorkbook().createFont();
+        style.setBorderTop(borderStyle);
+        style.setBorderRight(borderStyle);
+        style.setBorderBottom(borderStyle);
+        style.setBorderLeft(borderStyle);
+        font.setBold(true);
+        style.setFont(font);
+        cell.setCellStyle(style);
     }
 
 }
