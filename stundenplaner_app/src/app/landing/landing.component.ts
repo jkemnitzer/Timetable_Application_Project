a111315760d15f9f7a3029463b062788b1afe5c4
somayehatef7 <somayeh.atef.derakhshan@hof-university.de>
2021-11-24 19:45:07 +0100

    Landing rework

--- stundenplaner_app/src/app/landing/landing.component.ts
+++ stundenplaner_app/src/app/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router,
+    private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
   }
 
+  navigate():void{
+    this.router.navigate(['/programs'],{
+      relativeTo:this.activatedRoute
+    })
+  }
 }
