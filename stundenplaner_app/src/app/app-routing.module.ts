b5b892a9a36d0b9bf9f3ebdcc022dda73bd89cf5
phuget <patrick.huget@hof-university.de>
2021-11-30 19:57:46 +0100

    feature/6048_anzeigen_des_studenplans fixed-Merge

--- stundenplaner_app/src/app/app-routing.module.ts
+++ stundenplaner_app/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { ProgramOverviewComponent } from './programs/program-overview/program-overview.component';
-import { LecturesOverviewComponent } from './lectures/lectures-overview/lectures-overview.component';
-import { RoomsComponent } from './rooms/rooms.component';
-import { UsersComponent } from './users/users.component';
-import { ProfileComponent } from './profile/profile.component';
-import { LandingComponent } from './landing/landing.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ProgramOverviewComponent} from './programs/program-overview/program-overview.component';
+import {LecturesOverviewComponent} from './lectures/lectures-overview/lectures-overview.component';
+import {RoomsComponent} from './rooms/rooms.component';
+import {UsersComponent} from './users/users.component';
+import {ProfileComponent} from './profile/profile.component';
+import {LandingComponent} from './landing/landing.component';
 import {ShowTimetableComponent} from "./show-timetable/show-timetable.component";
 import {UserRoleOverviewComponent} from "./user-roles/user-role-overview/user-role-overview.component";
 
@@ -16,7 +16,7 @@ const routes: Routes = [
   { path: 'lectures', component: LecturesOverviewComponent },
   { path: 'rooms', component: RoomsComponent },
   { path: 'profile', component: ProfileComponent },
-  { path: 'user-role', component: UserRoleOverviewComponent }
+  { path: 'user-role', component: UserRoleOverviewComponent },
   { path: 'show-timetable', component: ShowTimetableComponent},
   { path: 'users', component: UsersComponent },
 
